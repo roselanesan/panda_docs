44250cb709219decf81954ab82f60ae64f6fe153
1intan99 <41572020+roselanesan@users.noreply.github.com>
2026-03-08 23:50:00 +0700

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,25 +2,35 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+    integrations: [starlight({
+        title: 'Panda Key System',
+        social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/roselanesan/panda_docs' }],
+        sidebar: [
+            {
+                label: 'VSS',
+                items: [
+                    { label: 'VSS Overview', slug: 'vss/vss-overview' },
+                ],
+            },
+            {
+                label: 'Client Libraries',
+                items: [
+                    { label: 'V4-HyperGuard', slug: 'client-libraries/v4-hyperguard' },
+                    { label: 'V3 Library', slug: 'client-libraries/v3-library' },
+                    { label: 'Legacy v2.5', slug: 'client-libraries/legacy-v2-5' },
+                ],
+            },
+            {
+                label: 'Web API',
+                items: [
+                    { label: 'Overview', slug: 'web-api/overview' },
+                    { label: 'Dashboard API', slug: 'web-api/dashboard-api' },
+                ],
+            },
+        ],
+		}), react()],
 });
